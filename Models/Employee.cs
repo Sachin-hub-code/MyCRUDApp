da233a780e7a7715909da009d6e07832709b58da
Ravi Kumar Kushwaha <ravikumarkush3@gmail.com>
2025-06-17 07:23:02 +0530

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace MyCRUDApp.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
